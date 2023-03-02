11179cc1b6104ff118df3c92aec764ebf0fd5d11
Hidy97 <csaladi.alexandra@gmail.com>
2023-03-02 15:13:17 +0100

    További listák létrehozása, táblázat létrehozása

--- script.js
+++ script.js
@@ -1,35 +1,85 @@
 window.addEventListener("load", init)
 
+
+
 function init() {
-    console.log("Helló");
-
-    const articleElem = document.querySelectorAll("#tarolo");
-    console.log(articleElem)
-    articleElem.innerHTML = "<h1>A legjobb dolgok</h1>";
-    const list = ["Végre meg tudtam akadályozni, hogy a kutya hátraszökjön a kertbe", "Tudtam ültetni és van két virágom", "Lebetegedtem", "Tegnapi adatbázis-kezelés", "Kupcsik tanárnő vicces", "Tegnap kialaudtam magam", "Sikerült Csolti Péteres feladatot teljesen megcsinálni! ", "Jó edzés volt a héten!",];
-
+    console.log("Hello világ");
+   const articleElem = document.getElementById('tarolo');
+   console.log(articleElem);
+    articleElem.innerHTML = "<h1> Legjobb dolgok </h1>";
+    const list = [
+        {
+            nev: "Maja",
+            mondat: "Végre meg tudtam akadályozni, hogy a kutya hátraszökjön a kertbe",
+        },
+        {
+            nev: "Szandi",
+            mondat: "Tudtam ültetni és van két virágom.",
+        },
+        { 
+            nev: "Trapi", 
+            mondat: "Sikerült a Feast 1 teszt!" 
+        }, 
+        { 
+            nev: "Ernő", 
+            mondat: "Tegnap kialaudtam magam," 
+        }, 
+        { 
+            nev: "Bálint", 
+            mondat: "Kupcsik tanarnő vicces volt," 
+        }, 
+        { 
+            nev: "Arnold", 
+            mondat: "Lebetegedtem", 
+        }, 
+        { 
+            nev: "Benedek", 
+            mondat: "Tegnapi adatbázis-kezelés", 
+        }, 
+        { 
+            nev: "Balázs", 
+            mondat: "Újra elövettem egy régi játékot, amivel már rég játszottam és nosztalgikus érzéseket vált ki.", 
+        }, 
+        { 
+            nev: "Duy", 
+            mondat: "Felfrissít az edzés minden héten." 
+        },
+    ];
     megjelenit(list, articleElem);
+    const Szuloelem = document.querySelectorAll("section");
+    megjelenit2(list, Szuloelem);
+    const tablazat = document.querySelectorAll("#tablazat")
+    tablazatbanMegjelenít(list, tablazat);
+    
 
-    const szuloElem = document.querySelectorAll("section");
-    megjelenit2(list, szuloElem);
 }
+
 
 function megjelenit(list, articleElem) {
-    let txt = "<ul>";
-    for (let index = 0; index < list.length; index++){
-        txt += "<li>" + list[index] + "</li>";
+    let txt = "<h1> Legjobb dolgok </h1>"
+    txt += "<ul>";
+    for (i=0; i<list.length; i++ ) {
+        txt +=  "<li>" + list[i].mondat  + "</li>";
     }
     txt += "</ul>";
-    articleElem[0].innerHTML += txt;
-    const articleListak = document.getElementsByClassName(".nev")
-    console.log(articleListak)
+    articleElem.innerHTML = txt;
 }
 
-function megjelenit2(list, szuloElem){
-    let text = "";
-    for (let index = 0; index < list.length; index++) {
-        text = "<div>" + "<p>" + list[index] + "</p>" + "</div>";
-    }
 
-    szuloElem[0].innerHTML += text;
+function megjelenit2(list, Szuloelem) {
+    let text = "";
+    for (i=0; i<list.length; i++ ) {
+        text += "<div>" +"<h3>"+ list[i].nev +"</h3>"+ "<p>" + list[i].mondat + "</p>" + "</div>";
+    }
+    Szuloelem[0].innerHTML += text;
+}
+
+function tablazatbanMegjelenít(list, tablazat){
+    let text = "<table>";
+    text += "<tr><th>Név</th><th>Mondat</th></tr>"
+    for (i=0; i<list.length; i++ ) {
+        text += "<tr>" +"<td>"+ list[i].nev +"</td>"+ "<td>" + list[i].mondat + "</td>" + "</<tr>";
+    }
+    tablazat[0].innerHTML += text;
+    text = "/table";
 }
